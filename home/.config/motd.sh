cb9109652bb941c2b7f6a791c54fcbe09b040668
Matt Spivey <spivey.matthew@gmail.com>
2018-12-30 11:45:21 -0500

    Add % signs

--- home/.config/motd.sh
+++ home/.config/motd.sh
@@ -1,4 +1,4 @@
-echo "  Battery: `cat /sys/class/power_supply/BAT0/capacity`%"
-echo "Backlight: `xbacklight -get | sed 's/^\([0-9]*\)\.[0-9]*/\1/'`%"
-echo "Freespace: `free -m | grep "^Mem" | awk '{printf("%i %iG/%iG", 100*$4/$2, $4, $2)}'`"
-echo "   Memory: `df -TPh / | tail -n1 | awk '{printf("%i %s/%s", $6, $4, $3)}'`"
+echo "    Battery: `cat /sys/class/power_supply/BAT0/capacity`%"
+echo "  Backlight: `xbacklight -get | sed 's/^\([0-9]*\)\.[0-9]*/\1/'`%"
+echo " Free space: `free -m | grep "^Mem" | awk '{printf("%i%% %iG/%iG", 100*$4/$2, $4, $2)}'`"
+echo "Free memory: `df -TPh / | tail -n1 | awk '{printf("%i%% %s/%s", $6, $4, $3)}'`"
